46a0f278d9f76274dbd5bf9d76bf334f7ecd46a9
pcfrati <fratipaula@gmail.com>
2025-05-14 11:29:12 -0300

    14.05.25

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ const articlesController = require("./articles/ArticlesController")
 
 const Article = require("./articles/Articles")
 const Category = require("./categories/Category")
-
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    Category.findAll()
+        .then(categories => {
+            res.locals.categories = categories;
+            next();
+        })
+        .catch(err => {
+            console.error("Erro ao carregar categorias no middleware:", err);
+            res.locals.categories = [];
+            next();
+        });
+});
 
 connection
     .authenticate()
@@ -26,16 +39,27 @@ connection
 app.use("/", categoriesController);
 app.use("/", articlesController);
 
+
 app.get("/", (req, res) => {
     Article.findAll({
         order: [['id', 'DESC']]
     }).then(articles => {
-        res.render("index", { articles: articles });
+        Category.findAll().then(categories => {  // Buscando categorias
+            res.render("index", { 
+                articles: articles,
+                categories: categories // Passando as categorias para a view
+            });
+        }).catch(err => {
+            console.log("Erro ao buscar categorias:", err);
+            res.redirect("/"); 
+        });
     }).catch(err => {
-        console.log(err);
+        console.log("Erro ao buscar artigos:", err);
         res.redirect("/"); 
     });
 });
+
+
 
 app.get("/:slug", (req,res) => {
     var slug = req.params.slug;
@@ -45,7 +69,9 @@ app.get("/:slug", (req,res) => {
         }
     }).then(article => {
         if(article != undefined){
-            res.render("article", {article:article});
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
         }else{
             res.redirect("/");
         }
@@ -53,6 +79,22 @@ app.get("/:slug", (req,res) => {
         res.redirect("/");
     });
 })
+
+app.get("/category/:slug", (req, res) => {
+    const slug = req.params.slug;
+    Category.findOne({
+        where: { slug },
+        include: [{ model: Article }]
+    }).then(category => {
+        if (category) {
+            res.render("index", { articles: category.articles });
+        } else {
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
 
 app.listen(4000, () => {
     console.log("o servidor está rodando")
